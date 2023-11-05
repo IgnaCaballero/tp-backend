2df8b74a74485d61b2e7e12cb5dec35094fe636b
IgnaCaballero <141593365+IgnaCaballero@users.noreply.github.com>
2023-11-05 02:33:36 -0300

    ...

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const productoRoutes = require('./routes/productoRoutes');
+const categoriaRoutes = require('./routes/categoriaRoutes')
+const ordenCompraRoutes = require('./routes/ordenCompraRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.get('/', (req, res) =>{
     res.send('Home')
 });
 
-app.get('/producto', );
-app.get('/categoria', );
-app.get('/orden', );
+app.get('/producto', productoRoutes);
+app.get('/categoria', categoriaRoutes);
+app.get('/orden', ordenCompraRoutes);
 
 mongoose
     .connect('mongodb+srv://ignacaballero:ingreso123@cluster0.spmrf7i.mongodb.net/productos?retryWrites=true&w=majority')
